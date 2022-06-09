ba0c3f7ae361487c90d2331ef89ad383ad390233
rarisenpai <elvorashs759@gmail.com>
2022-06-09 16:24:45 +0300

    An att.py that gives the attendant the ability to GET a list of all products or see a specific product using name

---
+++ att.py
@@ -0,0 +1,24 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+...
+in_memory_datastore = {
+   "rice": {"name": "RICE", "Category": "Food", "Quantity": 348},
+   "sunglasses": {"name": "SUNGLASSES", "Category": "Fashion", "Quantity": 324},
+   "milkshake": {"name": "MILKSHAKE", "Category": "Drinks", "Quantity": 431},
+   "scarf": {"name": "SCARF", "Category": "Clothing","Quantity": 312},
+   "cakes": {"name": "CAKES", "Category": "Food", "Quantity": 234},
+   "beans": {"name": "BEANS", "Category": "Food", "Quantity": 343},
+   "coffee": {"name": "COFFEE", "Category": "Drinks", "Quantity": 312},
+   "sportshoes": {"name": "SPORTSHOES", "Category":"Sportswear","Quantity": 214},
+   "soda": {"name": "SODA", "Category": "Drinks", "Quantity": 341},
+   "shirt": {"name": "SHIRT", "Category":"Clothing", "Quantity": 112},
+}
+if __name__ == '__main__': 
+    @app.get('/api/v1/products')
+    def list_products():
+        return {'products': list(in_memory_datastore.values())}
+
+    @app.get('/api/v1/products/<productsId>')
+    def get_products(productsId):
+        return in_memory_datastore[productsId]
