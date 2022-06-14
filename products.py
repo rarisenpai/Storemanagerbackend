4ef92646e19ad5977874ce1d535255acded62e61
rarisenpai <elvorashs759@gmail.com>
2022-06-14 15:14:37 +0300

    Created log in authentication based on flask http-auth Made a log in authenticator that grants user role based permissions in the app. I also separated data from logic. I am storing the data in an array of objects

---
+++ products.py
@@ -0,0 +1,12 @@
+products = [
+    {"rice": {"name": "RICE", "Category": "Food", "Quantity": 348}},
+    {"sunglasses": {"name": "SUNGLASSES", "Category": "Fashion", "Quantity": 324}},
+    {"milkshake": {"name": "MILKSHAKE", "Category": "Drinks", "Quantity": 431}},
+    {"scarf": {"name": "SCARF", "Category": "Clothing","Quantity": 312}},
+    {"cakes": {"name": "CAKES", "Category": "Food", "Quantity": 234}},
+    {"beans": {"name": "BEANS", "Category": "Food", "Quantity": 343}},
+    {"coffee": {"name": "COFFEE", "Category": "Drinks", "Quantity": 312}},
+    {"sportshoes": {"name": "SPORTSHOES", "Category":"Sportswear","Quantity": 214}},
+    {"soda": {"name": "SODA", "Category": "Drinks", "Quantity": 341}},
+    {"shirt": {"name": "SHIRT", "Category":"Clothing", "Quantity": 112}},
+]
